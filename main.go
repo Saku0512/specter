8390eaa7e6498ceedbafbad8927f05bde851212e
saku0512 <saku0512sec@gmail.com>
2026-03-23 16:15:16 +0900

    feat: version flag

--- main.go
+++ main.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
+
 	"github.com/Saku0512/specter/config"
 	"github.com/Saku0512/specter/server"
 )
 
+var version = "dev"
+
 func main() {
 	configPath := flag.String("c", "config.yaml", "path to config file")
 	port := flag.String("p", "8080", "port to listen on")
+	v := flag.Bool("v", false, "show version")
+	flag.BoolVar(v, "version", false, "show version")
 	flag.Parse()
+
+	if *v {
+		fmt.Println("specter", version)
+		os.Exit(0)
+	}
 
 	cfg, err := config.Load(*configPath)
 	if err != nil {
